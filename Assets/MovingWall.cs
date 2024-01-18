7a2ef5a5addc38f34535aa09918b29ba31e38254
jsa9292 <jsa9292@gmail.com>
2024-01-18 11:54:50 +0900

    fix

--- Assets/MovingWall.cs
+++ Assets/MovingWall.cs
@@ -7,11 +7,16 @@ public class MovingWall : MonoBehaviour
     public Vector3 dir;
     public float speed;
     public Transform pair;
+    public Transform[] onWall;
     // Start is called before the first frame update
     public void MoveWall()
     {
         transform.position += dir * speed * Time.deltaTime;
         pair.position += dir * speed * Time.deltaTime;
+        foreach (Transform t in onWall)
+        {
+            t.position += dir * speed * Time.deltaTime;
+        }
         Debug.Log("walls moving");
     }
 }
