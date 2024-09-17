3a4db6eb4b751bd616778f393dcf29b42be6ad87
insun0816 <jsa9292@gmail.com>
2024-09-18 03:09:32 +0900

    fixing wallcalib

--- Assets/MovingWall.cs
+++ Assets/MovingWall.cs
@@ -5,18 +5,19 @@ using UnityEngine;
 public class MovingWall : MonoBehaviour
 {
     public Vector3 dir;
+    public Vector3 worldDir;
     public float speed;
     public Transform pair;
     public Transform[] onWall;
     // Start is called before the first frame update
     public void MoveWall()
     {
-        transform.position += dir * speed * Time.deltaTime;
-        pair.position += dir * speed * Time.deltaTime;
+        worldDir = transform.TransformDirection(dir.x, dir.y, dir.z);
+        transform.position += worldDir * speed * Time.deltaTime;
+        pair.position += worldDir * speed * Time.deltaTime;
         foreach (Transform t in onWall)
         {
-            t.position += dir * speed * Time.deltaTime;
+            t.position += worldDir * speed * Time.deltaTime;
         }
-        Debug.Log("walls moving");
     }
 }
