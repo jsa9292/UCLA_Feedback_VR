57aaa2342aadf98e6182ea4fcb26328d7c9e192c
insun0816 <jsa9292@gmail.com>
2024-10-15 17:21:01 +0900

    vfx should work now

--- Assets/AudioAutoDisable.cs
+++ Assets/AudioAutoDisable.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class AudioAutoDisable : MonoBehaviour
 {
+    public Transform cam;
+    public VisualEffect vfx;
     public AudioSource aud;
     public float waitTime;
     public float playTime;
     // Update is called once per frame
     void Update()
     {
+        vfx.SetVector3("pos", cam.position);
+        vfx.SetVector3("rot", cam.localEulerAngles);
+        vfx.SetFloat("on", 1f);
         if (!aud.isPlaying)
         {
             playTime += Time.deltaTime;
+            vfx.SetFloat("on", 0f);
         }
         if (playTime > waitTime)
         {
